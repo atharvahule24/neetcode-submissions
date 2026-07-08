a0d6c9172ca04a4c89133e0abd28648636d20fd7
atharvahule24 <atharva.hule24@spit.ac.in>
2026-07-08 23:00:38 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return x;
+        }
+
+        if(n%2 == 0 && n > 0){
+            return myPow(x , n/2)*myPow(x,n/2);
+        }else if(n%2 != 0 && n > 0){
+            return x*myPow(x , n/2)*myPow(x,n/2);
+        }else if(n%2 == 0 && n < 0){
+            return 1/(myPow(x , Math.abs(n)/2)*myPow(x,Math.abs(n)/2));
+        }else{
+            return 1/(x*myPow(x , Math.abs(n)/2)*myPow(x,Math.abs(n)/2));
+        
+
+        // if(n < 0){
+        //     if(n%2 == 0){
+        //     return 1/myPow(x , n/2)*myPow(x,n/2);
+        // }else{
+        //     return 1/x*myPow(x , n/2)*myPow(x,n/2);
+        // }
+        }
+    }
+}
